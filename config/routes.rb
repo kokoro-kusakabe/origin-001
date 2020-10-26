98510f5643eda587b9ee2905ba96989ac34c057d
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-26 16:29:07 +0900

    アカウント登録の画面選択表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root to: "tweets#index"
   resources :users, only: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
