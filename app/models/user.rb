b289b24c01c730137af478b0fb94509fc450f651
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-26 16:37:32 +0900

    googleアカウント登録機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ApplicationRecord
 
   has_many :sns_credentials
   
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+  # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 end
