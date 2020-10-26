a39e31a8669cb6016d034d5da1c40b2bc53909dd
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-26 16:21:16 +0900

    モデルの作成とアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
+  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  
+  validates :nickname, presence: true
+
+  has_many :sns_credentials
+  
 end
