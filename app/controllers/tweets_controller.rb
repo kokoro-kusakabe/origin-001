32d954e8ff600acfc3accbd2994cd02b61e7f08f
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-27 17:10:40 +0900

    画像投稿機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,7 +19,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:title, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:title, :text, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,9 @@ class Tweet < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :text
+    validates :image
   end
 
   belongs_to :user
+  has_one_attached :image
 end
