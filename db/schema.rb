0715f3cac437ba71bc4faab6e903401662ceb116
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-27 16:18:21 +0900

    tweetモデルにバリデーション追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_071721) do
+ActiveRecord::Schema.define(version: 2020_10_27_070323) do
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_10_26_071721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_10_26_071721) do
   end
 
   add_foreign_key "sns_credentials", "users"
+  add_foreign_key "tweets", "users"
 end
