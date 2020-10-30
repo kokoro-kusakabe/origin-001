6531887e2c57d3da73ffd7a0c647a27672383d17
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-10-30 15:45:25 +0900

    ルーティングとコントラローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   }
   root to: "tweets#index"
   resources :users, only: :new
-  resources :tweets, only: [:new, :create]
+  resources :tweets, only: [:new, :create, :show]
 end
