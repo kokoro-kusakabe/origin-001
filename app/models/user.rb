25c5cffb7c0b5e73188c28a24b5ca83b363155c0
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-11-15 19:51:59 +0900

    relationshipモデルの作成とアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,17 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :comments
   has_many :likes, dependent: :destroy
-  has_many :liked_tweets, through: :likes, source: :tweet
+  
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+  has_many :liked_tweets, through: :likes, source: :tweet
+
   def already_liked?(tweet)
     likes.exists?(tweet_id: tweet.id)
   end
