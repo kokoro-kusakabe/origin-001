69315ded83fe295dd61d8a206a14203a5256ae0d
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-11-15 20:06:58 +0900

    コントローラーとルーティングの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
     @tweets = @user.tweets
@@ -23,4 +24,17 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
+  
 end
