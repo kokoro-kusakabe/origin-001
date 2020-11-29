388d5321222f2cdca7bd052c3d9680af648305bc
kokoro-kusakabe <kaleido4062aria.shana@gmail.com>
2020-11-29 16:34:50 +0900

    ArgumentError: Invalid option: harmonyの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = :uglifier
   
 end
